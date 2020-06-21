a85da4b1b813d07ea82e83c6db6bda032d297d7a
mahima-vs <mahima.gupta@vectoscalar.com>
2020-06-21 14:07:11 +0530

    Text editor code base.

--- examples/App.js
+++ examples/App.js
@@ -1,14 +1,20 @@
-/**
- * Sample React Native App
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Text
+} from 'react-native';
+import { WebView } from 'react-native-webview';
 
-import React from 'react';
+// import RNDraftJSRender from 'react-native-draftjs-renderer';
+// import contentState from 'DraftJs/contentState';
+ 
 
-export default class App extends React.Component {
-  render() {
-    return null;
+export default class App extends Component {
+  render () {
+    // return <Text> hello</Text>
+    return <WebView
+    source={{ uri: "http://localhost:3000/" }}
+  />
   }
 }
+
